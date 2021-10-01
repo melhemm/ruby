a30653949e7bb7534049e5bae45e06092c1e54f8
melhemm <jamalmulhemm@gmail.com>
2021-10-02 00:10:38 +0300

    set file name

--- ruby-part-4/main.rb
+++ ruby-part-4/main.rb
@@ -4,9 +4,9 @@ require './route'
 require './train'
 require './wagon'
 require './cargo_train'
-require './wagon_cargo'
+require './cargo_wagon'
 require './passenger_train'
-require './wagon_passenger'
+require './passenger_wagon'
 
 controller = MainMenu.new
 controller.start_program

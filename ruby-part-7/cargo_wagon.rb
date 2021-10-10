5c6110ad030eeecc742cd43060b8d62f6f6c59d6
melhemm <jamalmulhemm@gmail.com>
2021-10-10 14:52:44 +0300

    task-2-done

--- ruby-part-7/cargo_wagon.rb
+++ ruby-part-7/cargo_wagon.rb
@@ -1,6 +1,22 @@
 class CargoWagon < Wagon
-  def initialize(id)
-    super
+
+  def initialize(id, total_volume)
+    super(id)
+    @total_volume = total_volume
+    @capacity = 0
     @type = "cargo"
   end
+
+  def show_available_capacity
+    @capacity
+  end
+
+  def take_up_volume(volume)
+    @capacity += volume
+  end
+
+  def available_volume
+    @total_volume - @capacity
+  end
+
 end

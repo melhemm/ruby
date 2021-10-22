57a9aa12257c43e184381434b0ee8bd6c386c78a
melhemm <jamalmulhemm@gmail.com>
2021-10-22 18:31:01 +0300

    *arg error fixed

--- ruby-part-9/accessors.rb
+++ ruby-part-9/accessors.rb
@@ -1,25 +1,37 @@
 module Acсessors
-  def attr_accessor_with_history(*args)
-    args.each do |arg|
-      var_arg = "@#{arg}".to_sym
-      define_method(arg) { instance_variable_get(var_arg) }
+  def attr_accessor_with_history(*methods)
+    methods.each do |method|
+      var_name = "@#{method}".to_sym
+      history_var_name = "@#{method}_history".to_sym
+      history = []
 
-      define_method("#{arg}".to_sym) do |value|
-        instance_variable_set(args, value)
-        @history[args] ||= []
-        @history[args] << value
+      define_method(method) do
+        instance_variable_get(var_name)
       end
-      define_method("#{name}_history") { @history[args] }
+
+      define_method("#{method}=") do |v|
+        history << v
+        instance_variable_set(history_var_name, history)
+        instance_variable_set(var_name, v)
+      end
+
+      define_method("#{method}_history") do
+        instance_variable_get(history_var_name)
+      end
     end
   end
 
-  def strong_attr_accessor(arg, type)
-    var_arg = "@#{arg}".to_sym
-    define_method(arg) {instance_variable_get(var_arg)}
-  
-    define_method("#{arg}=".to_sym) do |value|
-      raise "type error" if value.is_a?(type)
-      instance_variable_set(var_arg, value)
+  def strong_attr_accessor(method, type)
+    var_name = "@#{method}".to_sym
+
+    define_method(method) do
+      instance_variable_get(var_name)
+    end
+
+    define_method("#{method}=") do |v|
+      raise TypeError unless v.instance_of? type
+
+      instance_variable_set(var_name, v)
     end
   end
 end
